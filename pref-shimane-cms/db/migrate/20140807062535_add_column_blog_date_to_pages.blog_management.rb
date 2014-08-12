43206b40ceed7f37a2f048902bf29dff3ec1f55f
Yusuke Anno <anno@netlab.jp>
2014-08-12 11:50:01 +0900

    エンジンのマイグレーションファイルを削除

--- pref-shimane-cms/db/migrate/20140807062535_add_column_blog_date_to_pages.blog_management.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from blog_management (originally 20131112045656)
-class AddColumnBlogDateToPages < ActiveRecord::Migration
-  def change
-    add_column :pages, :blog_date, :date
-  end
-end
