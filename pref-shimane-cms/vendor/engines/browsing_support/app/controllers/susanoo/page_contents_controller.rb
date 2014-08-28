c75aee465291bd75332d72ae45e95315e2bb8c39
Yusuke Anno <anno@netlab.jp>
2014-08-28 11:02:34 +0900

    ページコンテンツ編集中にルビを表示しないよう修正

--- pref-shimane-cms/vendor/engines/browsing_support/app/controllers/susanoo/page_contents_controller.rb
+++ pref-shimane-cms/vendor/engines/browsing_support/app/controllers/susanoo/page_contents_controller.rb
@@ -4,6 +4,6 @@ class Susanoo::PageContentsController < ApplicationController
   # html にルビをふる
   #
   # see app/controllers/application_controller.rb
-  after_action :rubi_filter, only: %i(content)
+#  after_action :rubi_filter, only: %i(content)
 
 end
