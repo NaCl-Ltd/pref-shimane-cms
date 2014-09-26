405ea607202bd84efec26ff0e22e42a52ea82e0e
Go Kitagawa <kida@netlab.jp>
2014-09-26 11:25:06 +0900

    リンク切れをチェックする機能を追加。

--- pref-shimane-cms/vendor/engines/consult_management/app/helpers/consult_management/consults_helper.rb
+++ pref-shimane-cms/vendor/engines/consult_management/app/helpers/consult_management/consults_helper.rb
@@ -1,5 +1,27 @@
 module ConsultManagement
   module ConsultsHelper
     include BootstrapFlashHelper
+
+    def lost_link_check_td_tag(consult)
+      td_tag = '<td>'
+      lost_link_td_tag = '<td style="background-color: red;">'
+
+      url = consult.link.gsub(Settings.public_uri.chop, "")
+      unless url =~ /^htt/
+        page = Page.find_by_path(url)
+        if page
+          visitor_content = page.visitor_content
+          unless visitor_content
+            td_tag = lost_link_td_tag
+          end
+        else
+          td_tag = lost_link_td_tag
+        end
+      end
+
+      td_tag += consult.name + '</td>'
+
+      return td_tag
+    end
   end
 end
